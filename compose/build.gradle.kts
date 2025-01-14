4a4e57ff13d76f2a544faa0058343ffec506532f
Mustafa Haddara <mustafa@honeycomb.io>
2025-01-14 11:39:50 -0500

    update version number for compose package

--- compose/build.gradle.kts
+++ compose/build.gradle.kts
@@ -18,7 +18,7 @@ android {
             minCompileSdk = 21
         }
 
-        buildConfigField("String", "HONEYCOMB_COMPOSE_DISTRO_VERSION", "\"0.0.1-alpha\"")
+        buildConfigField("String", "HONEYCOMB_COMPOSE_DISTRO_VERSION", "\"0.0.1-experimental2\"")
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         consumerProguardFiles("consumer-rules.pro")
@@ -100,7 +100,7 @@ publishing {
             create<MavenPublication>("release") {
                 groupId = "io.honeycomb.android"
                 artifactId = "honeycomb-opentelemetry-android-compose"
-                version = "0.0.1-alpha"
+                version = "0.0.1-experimental2"
 
                 afterEvaluate {
                     from(components["release"])
