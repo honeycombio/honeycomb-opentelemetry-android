83efaa18f60b5ac47f3b9efe3ded147f137a7b36
Bee Klimt <beeklimt@honeycomb.io>
2025-04-23 21:04:16 -0700

    remove commented out lines

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -37,8 +37,6 @@ android {
     }
     compileOptions {
         isCoreLibraryDesugaringEnabled = true
-        // sourceCompatibility = JavaVersion.VERSION_1_8
-        // targetCompatibility = JavaVersion.VERSION_1_8
     }
     kotlin {
         jvmToolchain(21)
