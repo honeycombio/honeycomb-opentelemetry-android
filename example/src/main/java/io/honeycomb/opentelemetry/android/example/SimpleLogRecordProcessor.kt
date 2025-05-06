c0750039dc5bf954591266daf55ee84ca8219126
Arriana Blais <arriblais@honeycomb.io>
2025-05-06 18:58:27 -0400

    fix formatting

--- example/src/main/java/io/honeycomb/opentelemetry/android/example/SimpleLogRecordProcessor.kt
+++ example/src/main/java/io/honeycomb/opentelemetry/android/example/SimpleLogRecordProcessor.kt
@@ -6,7 +6,10 @@ import io.opentelemetry.sdk.logs.LogRecordProcessor
 import io.opentelemetry.sdk.logs.ReadWriteLogRecord
 
 class SimpleLogRecordProcessor : LogRecordProcessor {
-    override fun onEmit(context: Context, logRecord: ReadWriteLogRecord) {
+    override fun onEmit(
+        context: Context,
+        logRecord: ReadWriteLogRecord,
+    ) {
         logRecord.setAttribute(AttributeKey.stringKey("app.metadata"), "extra metadata")
     }
 }
