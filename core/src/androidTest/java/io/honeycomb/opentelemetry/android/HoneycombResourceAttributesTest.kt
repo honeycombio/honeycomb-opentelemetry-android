9c04439e6c28b119af7fcd160c08bdde2a8fad4e
Wolfgang Therrien <wolfgangtherrien@honeycomb.io>
2025-09-22 14:16:35 -0500

    👾

--- core/src/androidTest/java/io/honeycomb/opentelemetry/android/HoneycombResourceAttributesTest.kt
+++ core/src/androidTest/java/io/honeycomb/opentelemetry/android/HoneycombResourceAttributesTest.kt
@@ -1,50 +1,21 @@
 package io.honeycomb.opentelemetry.android.example
 
 import android.os.Build
-import androidx.compose.ui.test.assertCountEquals
-import androidx.compose.ui.test.isDisplayed
-import androidx.compose.ui.test.junit4.createAndroidComposeRule
-import androidx.compose.ui.test.onAllNodesWithText
-import androidx.compose.ui.test.onNodeWithTag
-import androidx.compose.ui.test.onNodeWithText
-import androidx.compose.ui.test.performClick
-import androidx.compose.ui.text.intl.Locale
-import androidx.compose.ui.text.toUpperCase
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
-import androidx.test.uiautomator.By
-import androidx.test.uiautomator.BySelector
-import androidx.test.uiautomator.UiDevice
-import androidx.test.uiautomator.UiObject2
-import androidx.test.uiautomator.Until
 import io.honeycomb.opentelemetry.android.Honeycomb
 import io.honeycomb.opentelemetry.android.HoneycombOptions
 import io.opentelemetry.api.common.AttributeKey
 import io.opentelemetry.semconv.ServiceAttributes
-import org.junit.AfterClass
 import org.junit.Assert.assertEquals
 import org.junit.Assert.assertNotNull
 import org.junit.Assert.assertTrue
-import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-import java.io.File
-import kotlin.time.Duration.Companion.seconds
-import kotlin.time.DurationUnit
-
-// How long to wait for the UI to update between interactions.
-val UI_WAIT_TIMEOUT = 5.seconds
-
-// This is a special directory that will be saved to the build directory after the test finishes.
-const val ADDITIONAL_TEST_OUTPUT_DIRECTORY =
-    "/sdcard/Android/media/io.honeycomb.opentelemetry.android.example/additional_test_output"
 
 /**
- * Instrumented test, which will execute on an Android device.
  *
- * These tests are primarily intended to exercise demo functionality in the test app in order to
- * make it emit various telemetry signals. The signals will then be verified by the assertions in
- * smoke-tests/smoke-e2e.bats
+ * These tests are intended to exercise SKD initialization.
  */
 @RunWith(AndroidJUnit4::class)
 class HoneycombResourceAttributesTest {
